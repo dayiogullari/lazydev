426acc82275ca0d4ce2ed4fdf28c6f94b26ac1ac
Ahmed Zeyad <keyboord01@gmail.com>
2025-02-26 02:09:19 +0300

    fix(frontend): adjust next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	images: {
-		domains: ["avatars.githubusercontent.com"],
-	},
+  images: {
+    domains: ["avatars.githubusercontent.com"],
+  },
+  reactStrictMode: true,
+
+  onDemandEntries: {
+    pagesBufferLength: 2,
+  },
 };
 
 export default nextConfig;
